9f4fdd9515e58216eb253cc7fffe57a69060f804
S. Brent Faulkner <brent.faulkner@shopify.com>
2021-12-09 12:39:05 -0500

    configure iTerm to use shared config

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 brew tap homebrew/cask-fonts
 brew install --cask font-jetbrains-mono
 
+defaults write com.googlecode.iterm2 PrefsCustomFolder -string "$HOME/.dotfiles/iTerm/Preferences"
+defaults write com.googlecode.iterm2 LoadPrefsFromCustomFolder -bool true
+
 ln -s "$HOME/.dotfiles/iTerm/Scripts/AutoLaunch" "$HOME/Library/Application Support/iTerm2/Scripts/AutoLaunch"
 
 git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
