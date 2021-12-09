af70a2bbd671b319dc34effc813f3c9e32830986
S. Brent Faulkner <brent.faulkner@shopify.com>
2021-12-09 12:34:01 -0500

    fonts, etc.

--- install.sh
+++ install.sh
@@ -1,3 +1,9 @@
-ln -s ~/.dotfiles/.zshrc ~
+brew tap homebrew/cask-fonts
+brew install --cask font-jetbrains-mono
+
+ln -s "$HOME/.dotfiles/iTerm/Scripts/AutoLaunch" "$HOME/Library/Application Support/iTerm2/Scripts/AutoLaunch"
+
 git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
 ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+
+ln -s "$HOME/.dotfiles/.zshrc" "$HOME/.zshrc"
